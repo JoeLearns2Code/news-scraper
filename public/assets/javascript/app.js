ee4889be0e0f9786b3c1bdba2b807f8990338a0a
Joe Hutchinson <48301850+JoeLearns2Code@users.noreply.github.com>
2019-07-25 22:37:37 -0700

    Added functionality to handlebars page with app.js

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -1,15 +1,24 @@
 //Get articles as JSON
 $.getJSON("/articles", data => {
     for (let i = 0; i < data.length; i++) {
-        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>")
+        $("#articles").append("<p data-id='" + data[i]._id + "'>" + data[i].headline + "<br />" + data[i].summary + "<br />" + data[i].link + "</p>")
     }
 });
 
 
-//When scrape button is pressed, gather new article list
+//When scrape for articles button is pressed, gather new article list
 $(document).on("click", "#getArticles", () => {
-    $("#load").css("visibility", "visible");
-}) ;
+    $.ajax({
+        method: "GET",
+        url: "/scrape"
+    })
+    .then(function(data) {
+        // Log the response
+        console.log(data);
+        location.reload();
+    });
+    
+});
 
 
 //When clicking on an article(p-tag) notes can be added
@@ -24,20 +33,59 @@ $(document).on("click", "p", function () {
         url: "/articles/" + thisId,
 
     })
-    .then((data) => {
-        $("#notes").append("<h2>" + data.title + "</h2>");
-        $("#notes").append("<input id='titleinput' name='title' >");
-        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-        $("#notes").append("<button class='btn btn-secondary' data-id='" + data._id + "' id='savenote'>Save Note</button>");
-        $("#notes").append("<button class='btn btn-secondary' data-id='" + data._id + "' id='deletenote>Delete Note</button>")
+        .then((data) => {
+            $("#notes").append("<h2>" + data.title + "</h2>");
+            $("#notes").append("<input id='titleinput' name='title' >");
+            $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
+            $("#notes").append("<button class='btn btn-secondary' data-id='" + data._id + "' id='savenote'>Save Note</button>");
+            $("#notes").append("<button class='btn btn-secondary' data-id='" + data._id + "' id='deletenote>Delete Note</button>")
 
-        //If there is a note attached to the article
-        if (data.note) {
-            $("titleinput").val(data.note.title);
-            $("#bodyinput").val(data.note.body);
-        }
-    });
+            //If there is a note attached to the article
+            if (data.note) {
+                $("titleinput").val(data.note.title);
+                $("#bodyinput").val(data.note.body);
+            }
+        });
 });
 
+//Save a note
+$(document).on("click", "#savenote", function () {
+    //current id
+    const thisId = $(this).attr("data-id");
 
+    //AJAX post request
+    $.ajax({
+        method: "POST",
+        url: "/articles" + thisId,
+        data: {
+            title: $("#titleInput").val(),
+            body: $("#bodyInput").val()
+        }
+    })
+        .then((data) => {
+            $("#notes").empty();
+        })
+    $("#titleInput").val("");
+    $("#bodyInput").val("");
+});
+
+//Delete a note
+$(document).on("click", "#deletenote", function(event) {
+    //current id
+    const thisId = $(this).attr("data-id");
+
+    $.ajax({
+        method: "DELETE",
+        url: "/articles/" + thisId,
+        success(data) {},
+        error(err) {
+            console.log(err)
+        }
+    })
+    .then((data) => {
+        $("#titleInput").val("");
+        $("#bodyInput").val("");
+        $("#notes").empty()
+    })
+});
 
